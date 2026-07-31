515874fa35faeaf84beb73f0c20507a52ffcaf2a
rycreatestudio <rycreatestudio@gmail.com>
2026-07-30 20:45:12 -0400

    Create lesson-1-variables.js
    lesson-1-variables

---
+++ lesson-1-variables.js
@@ -0,0 +1,85 @@
+//Variables
+
+/* Example 1 - First Name, Last Name, and Age
+
+let firstName = "Ryan";
+let lastName = "Maharaj";
+
+let age = 30;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+*/
+
+//Example 2 - First Name, Job Title, Years of Experience, and learningJavaScript
+/*
+let firstName = "Ryan";
+let jobTitle = "QA Engineer";
+let yearsOfExperience = 5;
+let learningJavaScript = true;
+
+console.log(firstName);
+console.log(jobTitle);
+console.log(yearsOfExperience);
+console.log(learningJavaScript);
+
+console.log("My name is " + firstName);
+console.log("I work as a " + jobTitle);
+console.log("I have " + yearsOfExperience + " years of experience");    
+*/
+
+/* Example 3 - Favorite Game, Favorite Food, and Wants Remote Job
+let favoriteGame = "Call of Duty";
+let favoriteFood = "Pizza";
+let wantsRemoteJob = "Hell Yes";
+
+console.log(favoriteGame);
+console.log(favoriteFood);
+console.log(wantsRemoteJob);
+*/ 
+
+/* Example 4 - Current Salary, Goal Salary, Learning JavaScript, and Favorite Programming Language`
+let currentSalary = 100000;
+let goalSalary = 130000;
+let learningJavaScript = true;
+let favoriteProgrammingLanguage = 'JavaScript';
+
+console.log("Current Salary: " + currentSalary);
+console.log("Goal Salary: " + goalSalary);
+console.log("Learning JavaScript: " + learningJavaScript);
+console.log("Favorite Programming Language: " + favoriteProgrammingLanguage);
+*/
+/* 
+let age = 30;
+const country = "USA";
+
+console.log("Age: " + age);
+console.log("Country: " + country);
+*/
+
+/* Example 5 - Age and Language
+let age = 30;
+age = 31;
+
+const language = "JavaScript"
+language = "Python";
+
+console.log(age);
+console.log(language);
+*/
+
+/* Example 6 - Score
+let score = 10;
+
+console.log(score);
+
+score = score + 5;
+
+console.log(score);
+
+score = score * 2;
+
+console.log(score);
+*/
