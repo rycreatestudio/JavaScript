7e0a8f2275128b4fab67e13610159225f209226b
rycreatestudio <rycreatestudio@gmail.com>
2026-08-05 17:39:19 -0400

    Update lesson-12-higher-order-functions.js

---
+++ lesson-12-higher-order-functions.js
@@ -0,0 +1,47 @@
+/* function add(a, b) {
+
+    return a + b;
+
+}
+
+let answer = add(5, 3);
+
+console.log(answer); */
+
+/* function greet(name) {
+    return "Hello " + name
+}
+
+let message = greet("Ryan")
+console.log(message); */
+
+/* function login(username, password) {
+    return true;
+}
+
+let success = login ("Ryan", "1234")
+
+if (success) {
+    console.log("Login worked!");
+} */
+
+/* function greet(name) {
+
+    return "Hello " + name;
+
+}
+
+let message = greet("Ryan");
+
+console.log(message); */
+
+/* function double(number) {
+
+    return number * 2;
+
+}
+
+let result = double(10);
+
+console.log(result + 5); */
+
