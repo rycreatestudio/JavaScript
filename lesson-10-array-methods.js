bb991a9647831e7dd5a474fa20a0d61f87b269e5
rycreatestudio <rycreatestudio@gmail.com>
2026-08-05 13:28:55 -0400

    Update lesson-10-array-methods.js

---
+++ lesson-10-array-methods.js
@@ -0,0 +1,39 @@
+/*lesson-10-array-methods.js */
+
+/* example of using the push() method to add an element to the end of an array.
+
+let cars = ["Lexus", "Toyota"];
+cars.push("Mazda"); // adds "Mazda" to the end of the array
+
+console.log(cars); // ["Lexus", "Toyota", "Mazda"]
+
+*/
+
+/* example of using the pop() method to remove the last element from an array.
+
+let cars = ["Lexus", "Toyota", "Mazda"];
+cars.pop(); // removes the last element from the array
+
+console.log(cars); // ["Lexus", "Toyota"]
+
+*/
+
+/* example of using the shift() method to remove the first element from an array.
+
+let cars = ["Lexus", "Toyota", "Mazda"];
+
+cars.forEach(function(car) {
+    console.log(car);
+});
+
+*/
+
+/* example of using the unshift() method to add an element to the beginning of an array.
+let cars = ["Lexus", "Toyota"];
+
+let result = cars.push("Mazda");
+
+console.log(result);
+
+*/
+
